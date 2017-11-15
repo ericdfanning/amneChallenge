c41692a014d7e5e1b36818d485b5c1ad14dfb2a4
Eric F <ericdfanning@gmail.com>
2017-11-15 00:37:28 -0600

    tried using switch statements instead of if/else

--- testing.js
+++ testing.js
@@ -1,101 +1,101 @@
 var fs = require('fs');
-
-// ******* IF USED IRL, MEMOIZE THE FUNCTION TO IMPROVE SPEED BY CREATING CONSTANT TIME LOOKUP *******
  
 // change name of file to test different input
-var file = 'maxInput'
+const a = 'amneExampleInput'
+const b = 'input2'
+const c = 'maxInput'
+
+var file = c
 
 fs.readFile(file, 'utf8', function(err, contents) {
-	let firstLine = contents.split('\n')[0]
-	let n = firstLine.split(' ')[0]
-	let windoh = firstLine.split(' ')[1]
-	let prices = contents.split('\n')[1].split(' ')
+  let firstLine = contents.split('\n')[0]
+  let n = firstLine.split(' ')[0]
+  let windoh = firstLine.split(' ')[1]
+  let prices = contents.split('\n')[1].split(' ')
   solve(Number(n), Number(windoh), prices)
+
 });
 
+function solve(n, wind, price) {
+  var converted = helper(n, price)
+  var arr = []
+  var solution = ''
+  for (let i = 0; i < n; i++) {
 
-function solve(n, wind, prices) {
-	var solution = ''
-	// iterate over the prices array up until a full window can not be completed
-  for (let i = 0; i < prices.length - wind + 1; i++) {
-  	solution += helper(i, wind, prices.slice(i, i + wind))
-  	if (i !== prices.length - wind) { // if on the last value, disregard the new line for formats sake
-  		solution += '\n'
+  	switch(arr.length < wind - 1) {
+  		case true: {arr.push(converted[i])}; break;
+  		case false: {
+  			let countPos = 0
+  			let countNeg = 0
+  			let total = 0
+
+  			for (let i = 0; i < arr.length; i++) {
+  				switch(arr[i]) {
+  					case 1: {
+  						countPos++
+  						if (countNeg > 0) {
+  						  total += -((countNeg * countNeg + countNeg) / 2)
+  						  countNeg = 0
+  						} 
+  					}; break;
+
+  					case -1: {
+						  countNeg++
+						  if (countPos > 0) {
+						    total += (countPos * countPos + countPos) / 2
+						    countPos = 0
+						  } 
+						}; break;
+  				}
+
+  			  if (i === arr.length - 1) {
+
+  			    if (countPos) {
+  			      total += (countPos * countPos + countPos) / 2
+  			      solution += total + '\n'
+  			    } else if (countNeg) {
+  			      total += -((countNeg * countNeg + countNeg) / 2)
+  			      solution += total + '\n'
+  			    } else {
+  			      solution += total + '\n'
+  			    }
+  			  }
+  			}
+
+  			arr.shift()
+  			arr.push(converted[i])
+
+  		}; break;
   	}
   }
-  // write the answers to output file
-  fs.writeFile('output', solution, 'utf-8', function(err, data) {
+  // console.log(solution)
+  fs.writeFile('output', solution, function (err) {
     if (err) throw err;
-  })
+  });
+
 }
 
-function helper(index, wind, prices) {
-  let count = 0
-  let increasing = []
-  let decreasing = []
-  let total;
+function helper(n, prices) {
+    let converted = []
 
-	for (let i = 0; i < wind ; i++) {
-		// since fs.readFile() returns input prices as strings,
-		// convert home prices back to a number
-		prices[i] = Number(prices[i])
-		prices[i + 1] = Number(prices[i + 1])
+    for (let i = 0; i < n ; i++) {
 
-		if (prices[i + 1] > prices[i]) { // handle if trend becomes increasing
-			if (decreasing.length) { // since trend is now increasing, compute the previous decreasing trend
-				total = doTheMath(decreasing.length)
-				count -= total
-				decreasing = []
-			}
-			increasing.push(prices[i])
+      prices[i] = Number(prices[i])
+      prices[i + 1] = Number(prices[i + 1])
 
-		} else if (prices[i + 1] < prices[i]) { // handle if trend becomes decreasing
-			if (increasing.length) {// since trend is now decreasing, compute the previous increasing trend
-				// do the math here
-				total = doTheMath(increasing.length)
-				count += total
-				increasing = []
-			}
-			decreasing.push(prices[i])
-
-		} else if (prices[i + 1] === prices[i]) { // handle if there are identical consecutive numbers
-			// like above, compute range based on which trend was the last trend. 
-			if (increasing.length) {
-				total = doTheMath(increasing.length)
-				count += total
-				increasing = []
-			} else if (decreasing.length) {
-				total = doTheMath(decreasing.length)
-				count -= total
-				decreasing = []
-			}
-		}
-
-		if (i === wind - 1) { // handle the last window of the input array of prices. Same methods as above.
-			if (prices[i] > prices[i - 1]) {
-				increasing.push(prices[i])
-				total = doTheMath(increasing.length - 1)
-				count += total
-
-			} else if (prices[i] < prices[i - 1]) {
-				decreasing.push(prices[i])
-				total = doTheMath(decreasing.length - 1)
-				count -= total
-
-			}
-		}
-	}
-	// console.log(count)
-	return count
-}
-
-function doTheMath(start) {
-	// using math instead of nested for loops,
-	// compute the total amount of subsets using 
-	// the sum of natural numbers up to 'start'.
-	var total = start
-	for (var i = start - 1; i > 0; i--) {
-		total += i
-	}
-	return total
-}
+      if (i === n - 1) { 
+        return converted
+      }
+      
+      if (prices[i] > prices[i + 1]) { 
+        // -1
+        converted.push(-1)
+      } else if (prices[i] < prices[i + 1]) {
+        // 1
+        converted.push(1)
+      } else if (prices[i + 1] === prices[i + 1]) { 
+        // 0
+        converted.push(0)
+      }
+    }
+  }
