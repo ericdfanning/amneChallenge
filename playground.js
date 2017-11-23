95f70fd76205ffaf076b0fe4e49359704758deea
Eric F <ericdfanning@gmail.com>
2017-11-22 21:52:41 -0600

    separated ranges from first object value. Working solution for some cases

--- playground.js
+++ playground.js
@@ -4,7 +4,7 @@ const a = 'amneExampleInput'
 const b = 'input2'
 const c = 'maxInput'
 
-var file = a
+var file = b
 
 var solution = ''
 
@@ -18,15 +18,16 @@ fs.readFile(file, 'utf8', function(err, contents) {
 });
 
 function solve(n, wind, prices) {
-  let converted = helper(wind, prices.slice(0, wind))
-  let revamp = converted.revamp
+  let converted = helper(wind, prices)
+  let startingObject = makeFirstObject(wind, prices.slice(0, wind))
+  let revamp = startingObject
   let ranges = converted.ranges
   let count = 1
   solution += revamp.sum + '\n'
-// console.log('beginning solution', ranges)
+console.log('beginning solution', startingObject)
 // 7 13 47 12 23 89 4 56 113
   for (let i = wind; i < prices.length; i++) {
-  	// console.log('starting object', i, prices.length, revamp)
+  	console.log('starting object\n', i, prices.length, revamp)
   	let newObj = {}
   	if (revamp.begConvrtVal > 0) {
   		revamp.begnLength -= 1
@@ -39,7 +40,7 @@ function solve(n, wind, prices) {
   	}
 
   	if (revamp.begnLength >= 2) {
-  		// console.log('BEGINNING LENGTH IS 2 or GREATER.', 'length:', newObj.begnLength, newObj.sum)
+  		console.log('BEGINNING LENGTH IS 2 or GREATER.', 'length:', newObj.begnLength, newObj.sum)
   		newObj.begConvrtVal = doTheMath(newObj.begnLength - 1)
 
   		if (revamp.begConvrtVal > 0) {
@@ -55,22 +56,22 @@ function solve(n, wind, prices) {
   		}
 
   	} else if (revamp.begnLength === 1 && count < ranges.length) {
-  		// console.log('INSIDE index', count)
+  		console.log('INSIDE index', count)
 
   		newObj.begnLength = ranges[count].length
-  		// console.log('INSIDE RANGE & LENGTH', newObj.begnLength, ranges[count])
+  		console.log('INSIDE RANGE & LENGTH', newObj.begnLength, ranges[count])
 
   		let nexRng = ranges[count]
-  		// console.log('lkajsd;lkl;kajsdl;fkas    ---', nexRng[1], nexRng[0], nexRng)
+  		console.log('lkajsd;lkl;kajsdl;fkas    ---', nexRng[1], nexRng[0], nexRng)
   		if (nexRng[1] < nexRng[0]) { // check if incoming number is < || >
   			newObj.begConvrtVal = -doTheMath(newObj.begnLength - 1)
-  			// console.log('*********  less than ', newObj.begConvrtVal)
+  			console.log('*********  less than ', newObj.begConvrtVal)
   		} else if (nexRng[1] > nexRng[0]) {
   			newObj.begConvrtVal = doTheMath(newObj.begnLength - 1)
-  			// console.log('*********  greater than ', newObj.begConvrtVal)
+  			console.log('*********  greater than ', newObj.begConvrtVal)
   		} else {
   			newObj.begConvrtVal = revamp.begConvrtVal
-  			// console.log('*********  equal ', newObj.begConvrtVal)
+  			console.log('*********  equal ', newObj.begConvrtVal)
   		}
 
   		if (count < ranges.length - 1) {
@@ -78,27 +79,27 @@ function solve(n, wind, prices) {
   		}
 
   	} else {
-  		// console.log('IN THE LAND OF NOWHERE!!!!!!!!!!!')
+  		console.log('IN THE LAND OF NOWHERE!!!!!!!!!!!')
   		count++
   	}
   	// if (revamp.begConvrtVal === 1 || revamp.begConvrtVal ===  -1) {
-  		// console.log('BEFORE ASSIGNING TO SUM size one', revamp.sum, revamp.begConvrtVal, newObj.begConvrtVal)
+  	// 	console.log('BEFORE ASSIGNING TO SUM size one', revamp.sum, revamp.begConvrtVal, newObj.begConvrtVal)
   	// 	newObj.sum = revamp.sum - revamp.begConvrtVal
   		
   	// } else {
-  		// console.log('BEFORE ASSIGNING TO SUM', revamp.sum, revamp.begConvrtVal, newObj.begConvrtVal)
+  	// 	console.log('BEFORE ASSIGNING TO SUM', revamp.sum, revamp.begConvrtVal, newObj.begConvrtVal)
   	// 	newObj.sum = revamp.sum - revamp.begConvrtVal + newObj.begConvrtVal
   		
   	// }
 
-  	// console.log('REVAMP', Number(prices[i]), 'index', i, revamp, 'NEW SUM', newObj.sum, 'end length', revamp.endLength)
+  	console.log('REVAMP', Number(prices[i]), 'index', i, revamp, 'NEW SUM', newObj.sum, 'end length', revamp.endLength)
   	if (prices[i] < revamp.endNumber && revamp.endConvrtVal > 0) { // check if incoming number is < || >
   		newObj.endLength = 2
   		newObj.endConvrtVal = -doTheMath(newObj.endLength - 1)
   		// console.log('one sum', newObj.sum)
   		newObj.sum += newObj.endConvrtVal 
 
-  		// console.log('ONE', newObj.sum)
+  		console.log('ONE', newObj.sum)
   	} else if (Number(prices[i]) < revamp.endNumber && revamp.endConvrtVal < 0) {
 	  		if (ranges.length > 1) {
 		  		newObj.endLength = revamp.endLength + 1
@@ -107,14 +108,14 @@ function solve(n, wind, prices) {
 		  		newObj.sum += newObj.endConvrtVal 
 		  	} else {
 		  		if (count <= wind) {
-		  			// console.log('ADDED BEGN BACK ON inside two')
+		  			console.log('ADDED BEGN BACK ON inside two')
 		  			newObj.begnLength += 1
 		  			newObj.begConvrtVal = revamp.begConvrtVal
 		  			newObj.endLength = revamp.endLength
 		  			newObj.sum = revamp.endConvrtVal 
 		  			newObj.endConvrtVal = revamp.endConvrtVal
 		  		} else {
-			  		// console.log('ranges less than one ###')
+			  		console.log('ranges less than one ###')
 						newObj.endLength = revamp.endLength + 1
 						newObj.endConvrtVal = -doTheMath(newObj.endLength - 1)
 						newObj.sum += revamp.endConvrtVal * -1
@@ -126,7 +127,7 @@ function solve(n, wind, prices) {
 		  		newObj.begnLength = wind
 		  		newObj.begConvrtVal = newObj.endConvrtVal
 		  	}
-  		// console.log('TWO', newObj.sum)
+  		console.log('TWO', newObj.sum)
 
   	} else if (Number(prices[i]) > revamp.endNumber && revamp.endConvrtVal < 0) {
 
@@ -134,32 +135,32 @@ function solve(n, wind, prices) {
   		newObj.endConvrtVal = doTheMath(newObj.endLength - 1)
   		newObj.sum += newObj.endConvrtVal 
 
-  		// console.log('THREE', newObj.sum)
+  		console.log('THREE', newObj.sum)
 
   	} else if (Number(prices[i]) > revamp.endNumber && revamp.endConvrtVal > 0) {
-  		// console.log('length ....... ', revamp.endLength, ranges.length)
+  		console.log('length ....... ', revamp.endLength, ranges.length)
   		if (ranges.length > 1) {
   			newObj.endLength = revamp.endLength + 1
-  			// console.log('new length', newObj.endLength)
+  			console.log('new length', newObj.endLength)
   			newObj.endConvrtVal = doTheMath(newObj.endLength - 1)
-				// console.log('new converted', newObj.endConvrtVal)
+				console.log('new converted', newObj.endConvrtVal)
   			newObj.sum += newObj.endConvrtVal - revamp.endConvrtVal
   		} else {
   			if (count <= wind) {
   				newObj.begnLength += 1
   				newObj.begConvrtVal = revamp.begConvrtVal
-  				// console.log('ADDED BEGN BACK ON inside four', count, wind - 1)
+  				console.log('ADDED BEGN BACK ON inside four', count, wind - 1)
 					newObj.endLength = revamp.endLength
 					newObj.sum = revamp.endConvrtVal 
 					newObj.endConvrtVal = revamp.endConvrtVal
   			} else {
 	  			newObj.endLength = revamp.endLength + 1
-	  			// console.log('new length', newObj.endLength)
+	  			console.log('new length after', newObj.endLength)
 	  			newObj.endConvrtVal = doTheMath(newObj.endLength - 1)
-					// console.log('new converted', newObj.endConvrtVal)
+					console.log('new converted after', newObj.endConvrtVal)
 	  			newObj.sum += newObj.endConvrtVal - revamp.endConvrtVal
   			}
-  			// console.log('ranges less than one ###')
+  			console.log('ranges less than one ###')
 
   		}
 
@@ -168,21 +169,21 @@ function solve(n, wind, prices) {
   			newObj.begConvrtVal = newObj.endConvrtVal
   		}
 
-  		// console.log('FOUR', newObj.sum, newObj.endConvrtVal, newObj.endLength)
+  		console.log('FOUR', newObj.sum, newObj.endConvrtVal, newObj.endLength)
 
   	} else if (Number(prices[i]) === revamp.endNumber) {
   		newObj.endLength = revamp.endLength = 0
   		newObj.endConvrtVal = revamp.endConvrtVal
 
-  		// console.log('ZERO', newObj.sum)
+  		console.log('ZERO', newObj.sum)
   	}
   	newObj.endNumber = Number(prices[i])
   	solution += newObj.sum + '\n'
   	revamp = Object.assign({}, newObj)
-  	// console.log('ITERATION', newObj)
+  	console.log('ITERATION', newObj)
 
   }
-  // console.log('FINAL OUTPUT', solution)
+  console.log('FINAL OUTPUT', solution)
   fs.writeFile('output', solution, function (err) {
     if (err) throw err;
   });
@@ -193,112 +194,311 @@ function helper(wind, prices) {
   let count = 0
   let increasing = []
   let decreasing = []
-  let total;
+  let total = 0
   let revamp = {}
   let ranges = []
+  //***** try changing the increasing and decreasing arrays into integers since I only need the length
+
+  for (let i = 0; i < prices.length ; i++) {
+
+    if (Number(prices[i + 1]) > Number(prices[i])) { 
+      if (decreasing.length) { 
+        // total = -doTheMath(decreasing.length)
+        if (i < wind) {
+          total += -doTheMath(decreasing.length)
+
+          count += total
+        }
+        // count -= total
+        decreasing.push(Number(prices[i]))
+        ranges.push(decreasing)
+        if (!revamp.begnLength) {
+          revamp.begConvrtVal = total
+          revamp.begnLength = decreasing.length
+        }
+
+        decreasing = []
+      }
+
+
+      if (i === wind - 1) {
+        // console.log('finished', decreasing.length, increasing.length)
+        if (increasing.length) {
+          total += doTheMath(increasing.length)
+        } else if (decreasing.length) {
+          total += -doTheMath(decreasing.length)
+        }
+        // console.log('finished', total)
+        if (!revamp.begnLength) {
+
+          revamp.begConvrtVal = total
+          revamp.begnLength = decreasing.length
+        }
+        revamp.endConvrtVal = total
+        revamp.endLength = decreasing.length
+        revamp.endNumber = Number(prices[i])
+        increasing = []
+      }
+      increasing.push(Number(prices[i]))
+
+    } else if (Number(prices[i + 1]) < Number(prices[i])) { 
+      if (increasing.length) {
+        // total = doTheMath(increasing.length)
+        if (i < wind) {
+          total += doTheMath(increasing.length)
+          count += total
+        }
+        // count += total
+        increasing.push(Number(prices[i]))
+        ranges.push(increasing)
+        if (!revamp.begnLength) {
+          revamp.begConvrtVal = total
+          revamp.begnLength = increasing.length
+        }
+
+
+        increasing = []
+      }
+
+
+      if (i === wind - 1) {
+        if (increasing.length) {
+          total += doTheMath(increasing.length)
+        } else if (decreasing.length) {
+          total += -doTheMath(decreasing.length)
+        }
+        if (!revamp.begnLength) {
+          revamp.begConvrtVal = total
+          revamp.begnLength = increasing.length
+        }
+        revamp.endConvrtVal = total
+        revamp.endLength = increasing.length
+        revamp.endNumber = Number(prices[i])
+        decreasing = []
+      }
+      decreasing.push(Number(prices[i]))
+
+    } else if (Number(prices[i + 1]) === Number(prices[i])) {
+
+      if (increasing.length) {
+        if (i < wind) {
+          total = doTheMath(increasing.length)
+          count += total
+        }
+        // count += total
+        increasing.push(Number(prices[i]))
+        ranges.push(increasing)
+        if (!revamp.begnLength) {
+          revamp.begConvrtVal = total
+          revamp.begnLength = increasing.length
+        } 
+
+        if (i === wind - 1) {
+          if (increasing.length) {
+            total += doTheMath(increasing.length)
+          } else if (decreasing.length) {
+            total += -doTheMath(decreasing.length)
+          }
+          if (!revamp.begnLength) {
+
+            revamp.begConvrtVal = total
+            revamp.begnLength = decreasing.length
+          }
+          revamp.endConvrtVal = total
+          revamp.endLength = decreasing.length
+          revamp.endNumber = Number(prices[i])
+          increasing = []
+        }
+        increasing.push(Number(prices[i]))
+        increasing = []
+      } else if (decreasing.length) {
+        if (i < wind) {
+          total = -doTheMath(decreasing.length)
+          count += total
+        }
+        // count -= total
+        ranges.push(decreasing)
+        if (!revamp.begnLength) {
+          revamp.begConvrtVal = total
+          revamp.begnLength = decreasing.length
+        }
+
+        if (i === wind - 1) {
+          if (increasing.length) {
+            total += doTheMath(increasing.length)
+          } else if (decreasing.length) {
+            total += -doTheMath(decreasing.length)
+          }
+          if (!revamp.begnLength) {
+            revamp.begConvrtVal = total
+            revamp.begnLength = increasing.length
+          }
+          revamp.endConvrtVal = total
+          revamp.endLength = increasing.length
+          revamp.endNumber = Number(prices[i])
+          decreasing = []
+        }
+        decreasing.push(Number(prices[i]))
+        decreasing = []
+      }
+    }
+
+    if (i === prices.length - 1) { 
+      if (Number(prices[i]) > prices[i - 1]) {
+        increasing.push(Number(prices[i]))
+        // total = doTheMath(increasing.length - 1)
+        if (i < wind) {
+          count += total
+        }
+        // if (i === wind - 1) {
+        //   total += doTheMath(increasing.length)
+        // }
+        // count += total
+        
+        // if (!revamp.begnLength) {
+        //   revamp.begConvrtVal = total
+        //   revamp.begnLength = increasing.length
+        // }
+        ranges.push(increasing)
+        // revamp.endConvrtVal = total
+        // revamp.endLength = increasing.length
+        // revamp.endNumber = Number(prices[i])
+
+      } else if (Number(prices[i]) < prices[i - 1]) {
+        decreasing.push(Number(prices[i]))
+        // total = doTheMath(decreasing.length - 1)
+        if (i < wind) {
+          count += total
+        }
+        // count -= total
+        
+        // if (!revamp.begnLength) {
+
+        //   revamp.begConvrtVal = total
+        //   revamp.begnLength = decreasing.length
+        // }
+        ranges.push(decreasing)
+        // revamp.endConvrtVal = total
+        // revamp.endLength = decreasing.length
+        // revamp.endNumber = Number(prices[i])
+      }
+    }
+  }
+
+  revamp.sum = total
+  return { revamp, ranges }
+}
+
+function makeFirstObject(wind, prices) {
+  let count = 0
+  let increasing = []
+  let decreasing = []
+  let total;
+  let revamp = {}
+
 
   //***** try changing the increasing and decreasing arrays into integers since I only need the length
 
-	for (let i = 0; i < wind ; i++) {
+  for (let i = 0; i < prices.length ; i++) {
 
-		prices[i] = Number(prices[i])
-		prices[i + 1] = Number(prices[i + 1])
+    prices[i] = Number(prices[i])
 
-		if (prices[i + 1] > prices[i]) { 
-			if (decreasing.length) { 
-				total = doTheMath(decreasing.length)
-				count -= total
-				decreasing.push(prices[i])
-				ranges.push(decreasing)
-				if (!revamp.begnLength) {
-					revamp.begConvrtVal = total
-					revamp.begnLength = decreasing.length
-				}
+    if (Number(prices[i + 1]) > prices[i]) { 
+      if (decreasing.length) { 
+        total = doTheMath(decreasing.length)
+        count -= total
+        decreasing.push(prices[i])
 
-				decreasing = []
-			}
-			increasing.push(prices[i])
+        if (!revamp.begnLength) {
+          revamp.begConvrtVal = total
+          revamp.begnLength = decreasing.length
+        }
 
-		} else if (prices[i + 1] < prices[i]) { 
-			if (increasing.length) {
+        decreasing = []
+      }
+      increasing.push(prices[i])
 
-				total = doTheMath(increasing.length)
-				count += total
-				increasing.push(prices[i])
-				ranges.push(increasing)
-				if (!revamp.begnLength) {
-					revamp.begConvrtVal = total
-					revamp.begnLength = increasing.length
-				}
+    } else if (Number(prices[i + 1]) < prices[i]) { 
+      if (increasing.length) {
 
-				increasing = []
-			}
-			decreasing.push(prices[i])
+        total = doTheMath(increasing.length)
+        count += total
+        increasing.push(prices[i])
 
-		} else if (prices[i + 1] === prices[i]) {
+        if (!revamp.begnLength) {
+          revamp.begConvrtVal = total
+          revamp.begnLength = increasing.length
+        }
 
-			if (increasing.length) {
-				total = doTheMath(increasing.length)
-				count += total
-				increasing.push(prices[i])
-				ranges.push(increasing)
-				if (!revamp.begnLength) {
-					revamp.begConvrtVal = total
-					revamp.begnLength = increasing.length
-				} 
-				increasing.push(prices[i])
-				increasing = []
-			} else if (decreasing.length) {
-				total = doTheMath(decreasing.length)
-				count -= total
-				decreasing.push(prices[i])
-				ranges.push(decreasing)
-				if (!revamp.begnLength) {
+        increasing = []
+      }
+      decreasing.push(prices[i])
 
-					revamp.begConvrtVal = total
-					revamp.begnLength = decreasing.length
-				}
-				decreasing.push(prices[i])
-				decreasing = []
-			}
-		}
+    } else if (Number(prices[i + 1]) === prices[i]) {
 
-		if (i === wind - 1) { 
-			if (prices[i] > prices[i - 1]) {
-				increasing.push(prices[i])
-				total = doTheMath(increasing.length - 1)
-				count += total
-				
-				if (!revamp.begnLength) {
+      if (increasing.length) {
+        total = doTheMath(increasing.length)
+        count += total
+        increasing.push(prices[i])
 
-					revamp.begConvrtVal = total
-					revamp.begnLength = increasing.length
-				}
-				ranges.push(increasing)
-				revamp.endConvrtVal = total
-				revamp.endLength = increasing.length
-				revamp.endNumber = prices[i]
+        if (!revamp.begnLength) {
+          revamp.begConvrtVal = total
+          revamp.begnLength = increasing.length
+        } 
+        increasing.push(prices[i])
+        increasing = []
+      } else if (decreasing.length) {
+        total = doTheMath(decreasing.length)
+        count -= total
+        decreasing.push(prices[i])
 
-			} else if (prices[i] < prices[i - 1]) {
-				decreasing.push(prices[i])
-				total = doTheMath(decreasing.length - 1)
-				count -= total
-				
-				if (!revamp.begnLength) {
+        if (!revamp.begnLength) {
 
-					revamp.begConvrtVal = total
-					revamp.begnLength = decreasing.length
-				}
-				ranges.push(decreasing)
-				revamp.endConvrtVal = total
-				revamp.endLength = decreasing.length
-				revamp.endNumber = prices[i]
-			}
-		}
-	}
+          revamp.begConvrtVal = total
+          revamp.begnLength = decreasing.length
+        }
+        decreasing.push(prices[i])
+        decreasing = []
+      }
+    }
 
-	revamp.sum = count
-	return { revamp, ranges }
+    if (i === wind - 1) { 
+      console.log('lkajsd', i, prices[i])
+      if (prices[i] > prices[i - 1]) {
+        increasing.push(prices[i])
+        total = doTheMath(increasing.length - 1)
+        count += total
+        
+        if (!revamp.begnLength) {
+
+          revamp.begConvrtVal = total
+          revamp.begnLength = increasing.length
+        }
+
+        revamp.endConvrtVal = total
+        revamp.endLength = increasing.length
+        revamp.endNumber = prices[i]
+
+      } else if (prices[i] < prices[i - 1]) {
+        decreasing.push(prices[i])
+        total = doTheMath(decreasing.length - 1)
+        count -= total
+        
+        if (!revamp.begnLength) {
+
+          revamp.begConvrtVal = total
+          revamp.begnLength = decreasing.length
+        }
+
+        revamp.endConvrtVal = -total
+        revamp.endLength = decreasing.length
+        revamp.endNumber = prices[i]
+      }
+    }
+  }
+
+  revamp.sum = count
+  return revamp
 }
 
 function doTheMath(start) {
--- testing.js
+++ testing.js
@@ -5,100 +5,129 @@ const a = 'amneExampleInput'
 const b = 'input2'
 const c = 'maxInput'
 
-var file = b
+var file = a
 
 fs.readFile(file, 'utf8', function(err, contents) {
-	let firstLine = contents.split('\n')[0]
-	let n = firstLine.split(' ')[0]
-	let windoh = firstLine.split(' ')[1]
-	let prices = contents.split('\n')[1].split(' ')
+  let firstLine = contents.split('\n')[0]
+  let n = firstLine.split(' ')[0]
+  let windoh = firstLine.split(' ')[1]
+  let prices = contents.split('\n')[1].split(' ')
   solve(Number(n), Number(windoh), prices)
+
 });
 
-
 function solve(n, wind, prices) {
-	var solution = ''
-	// iterate over the prices array up until a full window can not be completed
-  for (let i = 0; i < prices.length - wind + 1; i++) {
-  	solution += helper(i, wind, prices.slice(i, i + wind))
-  	if (i !== prices.length - wind) { // if on the last value, disregard the new line for formats sake
-  		solution += '\n'
+  var converted = helper(n, prices)
+  console.log('converted', converted)
+  wind = wind - 1
+  var solution = ''
+
+  var total = 0
+  var obj = {}
+  var count = 0
+  var indexOfConverted = 0
+  for (let j = 0; j < (n - (wind + 1) + 1); j++) {
+
+  	for (let i = 0; i < converted.length; i++) {
+  		if (total === wind) {
+  			break
+  		} else {
+	  		if (Math.abs(converted[i]) > wind - total) {
+	  			let b = wind - total
+	  			total += wind - total
+	  			if (converted[i] > 0) {
+	  			  obj[count] = (b * b + b) / 2
+	  			} else {
+	  				obj[count] = -((b * b + b) / 2)
+	  			}
+	  		} else {
+	  			total += Math.abs(converted[i])
+	  			let b = Math.abs(converted[i])
+	  			if (converted[i] > 0) {
+	  			  obj[count] = (b * b + b) / 2
+	  			} else {
+	  				obj[count] = -((b * b + b) / 2)
+	  			}
+	  		}
+  		}
+  		count++
   	}
-  }
-  // write the answers to output file
-  console.log
-  fs.writeFile('output', solution, 'utf-8', function(err, data) {
+  	console.log('IT RAN', j)
+
+		let sum = 0
+
+		for (let key in obj) {
+			sum += obj[key]
+		}
+
+	  solution += '\n' + sum
+
+		total = 0
+		count = 0
+
+		if (Math.abs(converted[indexOfConverted]) === 0) {
+			indexOfConverted++
+		} 
+
+		if (converted[indexOfConverted] > 0) {
+			converted[indexOfConverted] = converted[indexOfConverted] - 1
+		} else if (converted[indexOfConverted] < 0) {
+			converted[indexOfConverted] = converted[indexOfConverted] + 1
+		}
+	}
+  // console.log(solution)
+
+  fs.writeFile('output', solution, function (err) {
     if (err) throw err;
-  })
+  });
+
 }
 
-function helper(index, wind, prices) {
-  let count = 0
-  let increasing = []
-  let decreasing = []
-  let total;
+function helper(n, prices) {
+    let converted = []
+    let solution = []
+    for (let i = 0; i < n ; i++) {
 
-	for (let i = 0; i < wind ; i++) {
-		// since fs.readFile() returns input prices as strings,
-		// convert home prices back to a number
-		prices[i] = Number(prices[i])
-		prices[i + 1] = Number(prices[i + 1])
+      prices[i] = Number(prices[i])
+      prices[i + 1] = Number(prices[i + 1])
 
-		if (prices[i + 1] > prices[i]) { // handle if trend becomes increasing
-			if (decreasing.length) { // since trend is now increasing, compute the previous decreasing trend
-				total = doTheMath(decreasing.length)
-				count -= total
-				decreasing = []
-			}
-			increasing.push(prices[i])
+      if (i === n - 1) { 
 
-		} else if (prices[i + 1] < prices[i]) { // handle if trend becomes decreasing
-			if (increasing.length) {// since trend is now decreasing, compute the previous increasing trend
-				// do the math here
-				total = doTheMath(increasing.length)
-				count += total
-				increasing = []
-			}
-			decreasing.push(prices[i])
+      	let countPos = 0
+      	let countNeg = 0
 
-		} else if (prices[i + 1] === prices[i]) { // handle if there are identical consecutive numbers
-			// like above, compute range based on which trend was the last trend. 
-			if (increasing.length) {
-				total = doTheMath(increasing.length)
-				count += total
-				increasing = []
-			} else if (decreasing.length) {
-				total = doTheMath(decreasing.length)
-				count -= total
-				decreasing = []
-			}
-		}
-
-		if (i === wind - 1) { // handle the last window of the input array of prices. Same methods as above.
-			if (prices[i] > prices[i - 1]) {
-				increasing.push(prices[i])
-				total = doTheMath(increasing.length - 1)
-				count += total
-
-			} else if (prices[i] < prices[i - 1]) {
-				decreasing.push(prices[i])
-				total = doTheMath(decreasing.length - 1)
-				count -= total
-
-			}
-		}
-	}
-	// console.log(count)
-	return count
-}
-
-function doTheMath(start) {
-	// using math instead of nested for loops,
-	// compute the total amount of subsets using 
-	// the sum of natural numbers up to 'start'.
-	var total = start
-	for (var i = start - 1; i > 0; i--) {
-		total += i
-	}
-	return total
-}
+	      for (let i = 0; i < converted.length; i++) {
+	      	if (converted[i] === 1) {
+	      	  countPos++
+	      	  if (countNeg > 0) {
+	      	    solution.push(-countNeg)
+	      	    countNeg = 0
+	      	  } 
+	      	} else if (converted[i] === -1 ) {
+	      	  countNeg++
+	      	  if (countPos > 0) {
+	      	    solution.push(countPos)
+	      	    countPos = 0
+	      	  } 
+	      	}
+	      }
+	      if (countPos) {
+	        solution.push((countPos * countPos + countPos) / 2)
+	      } else if (countNeg) {
+	      	solution.push(-((countNeg * countNeg + countNeg) / 2))
+	      }
+        return solution
+      }
+      
+      if (prices[i] > prices[i + 1]) { 
+        // -1
+        converted.push(-1)
+      } else if (prices[i] < prices[i + 1]) {
+        // 1
+        converted.push(1)
+      } else if (prices[i + 1] === prices[i + 1]) { 
+        // 0
+        converted.push(0)
+      }
+    }
+  }
